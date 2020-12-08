1c69b8712363ec12d4f8869ee0df2e83215f6aec
IvanJoskic <i.joskic@outlook.com>
2020-12-08 23:26:08 +0100

    Day 6

---
+++ day-six.py
@@ -0,0 +1,26 @@
+inputFile = open('input6.txt')
+
+try:
+    lines = inputFile.read()
+
+    totalA = totalB = 0
+    groups = lines.split('\n\n')
+
+    for group in groups:
+        answers = group.replace('\n', '')
+        uniqueAnswers = set(answers)
+        totalA += len(uniqueAnswers)
+        numOfPeople = len(group.split('\n'))
+        # print(answers, len(uniqueAnswers), numOfPeople)
+
+        for letter in uniqueAnswers:
+            if answers.count(letter) == numOfPeople:
+                totalB += 1
+                # print(group, numOfPeople, letter, '\n')
+            else:
+                print(group, numOfPeople, letter, '\n')
+
+    print("Total number of answers anyone said 'Yes' to: " + str(totalA))
+    print("Total number of answers everyone said 'Yes' to: " + str(totalB))
+finally:
+    inputFile.close()
